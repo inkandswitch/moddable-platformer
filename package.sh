7bf0442c4925aa0a6dd0d833e5466d603a5b5847
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-12-10 09:52:54 +0100

    tweak package command

--- package.sh
+++ package.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cargo build --target x86_64-pc-windows-gnu
+cargo build --target x86_64-pc-windows-gnu || exit 1
+cargo build --target aarch64-apple-darwin || exit 1
 
 # Create export directory
 export_dir="/tmp/godot-export"
@@ -19,9 +20,12 @@ mkdir -p "$export_dir/target/x86_64-pc-windows-gnu/debug"
 # Copy only top-level Windows debug files
 find ./target/x86_64-pc-windows-gnu/debug -maxdepth 1 -type f -exec cp {} "$export_dir/target/x86_64-pc-windows-gnu/debug/" \;
 
+# Copy only top-level debug files
+mkdir -p "$export_dir/target/debug"
+find ./target/debug -maxdepth 1 -type f -exec cp {} "$export_dir/target/debug/" \;
+
 
 # zip up project in tmp folder
-
 cd "$export_dir"
 zip -r "patchwork.zip" ./*
 
