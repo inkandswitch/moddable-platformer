68db53e5c43b0a673e32345c768c7959bceed873
Paul Sonnentag <paul.sonnentag@googlemail.com>
2024-12-16 16:18:27 +0100

    build all targets in run

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 # Build the Rust plugin
-cd ./addons/patchwork/rust
-cargo build || exit 1
+cargo build --target aarch64-apple-darwin || exit 1
+cargo build --target x86_64-apple-darwin || exit 1
+cargo build --target x86_64-pc-windows-gnu || exit 1
+cargo build --target x86_64-unknown-linux-gnu || exit 1
 
-# Change back to root directory and open Godot project
-cd "../../.."
 
 # Rust godot needs GODOT4_BIN
 GODOT4_BIN=/Applications/Godot.app/Contents/MacOS/Godot 
